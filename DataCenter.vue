4cb0dc615ad25aed14793d79b66a0a19ea10a38b
515530481 <93919816+515530481@users.noreply.github.com>
2023-03-27 08:42:56 +0800

    Add files via upload

--- DataCenter.vue
+++ DataCenter.vue
@@ -19,7 +19,7 @@
       </div>
       <div class="kuang1">
         <div class="biaotou-1">
-          <table class="table-1" :data="data">
+          <table class="table-1" >
             <thead>
               <tr>
                 <th
@@ -33,19 +33,21 @@
               </tr>
             </thead>
             <tbody class="body">
-              <tr v-for="(data, index) in dataSource" :key="index" class="tr">
+              <tr v-for="(data, index) in dataSource" :key="index" >
+                
                 <td
                   v-for="(col, ind) in columns"
+                  :width="col.width"
+                  height="30px"
                   :key="ind"
-                  :title="data[col.dataIndex]"
-                  class="td"
-                >
-                  <!-- <template slot-scope="scope" slot="edit">
-                   <el-button type="primary" icon="el-icon-edit" size="small" ></el-button>
-                    </template>
-                    <template slot-scope="scope" slot="delete">
-                    <el-button type="danger" icon="el-icon-delete" size="small" ></el-button>
-                    </template> -->
+                 
+                >{{data[col.dataIndex]}}
+                 <slot v-if="col.slot==1" v-bind:data="data" >
+                  <el-button type="primary" icon="el-icon-edit" size="small" ></el-button>
+                 </slot>
+                 <slot v-if="col.slot==2" v-bind:data="data" >
+                  <el-button type="danger" icon="el-icon-delete" size="small" ></el-button>
+                 </slot>
                 </td>
               </tr>
             </tbody>
@@ -56,13 +58,59 @@
     </div>
     <div class="biaozhun">
       <div class="biaozhun-top">
-        <div class="title"></div>
-        <div class="search-2"></div>
-        <div class="upload-2"></div>
-        <div class="export-2"></div>
-        <div class="new-2"></div>
+        <div class="title">ESD标准</div>
+        <div class="search-2">
+           <i class="el-icon-search"></i> Search
+          <i class="el-icon-search"></i>
+        </div>
+        <div class="upload-2">
+           <i class="el-icon-upload"> Upload</i>
+        </div>
+        <div class="export-2">
+           <i class="el-icon-download"> Export</i>
+        </div>
+        <div class="new-2">
+           <i class="el-icon-plus"> New</i>
+        </div>
       </div>
-      <div class="kuang2"></div>
+      <div class="kuang2">
+         <div class="biaotou-1">
+          <table class="table-1" cellspacing="0" cellspace="0">
+            <thead>
+              <tr>
+                <th
+                  :width="item.width"
+                  v-for="(item, index) in columns2"
+                  :key="index"
+                  class="th"
+                >
+                  {{ item.title }}
+                </th>
+              </tr>
+            </thead>
+            <tbody class="body">
+              <tr v-for="(data, index) in dataSource2" :key="index" :class="'tr-color-'+index%2" >
+                
+                 <td
+                  v-for="(col, ind) in columns2"
+                  :width="col.width"
+                  height="30px"
+                  :key="ind"
+                 
+                >{{data[col.dataIndex]}}
+                 <slot v-if="col.slot==1" v-bind:data="data" >
+                  <el-button type="primary" icon="el-icon-edit" size="small" ></el-button>
+                 </slot>
+                 <slot v-if="col.slot==2" v-bind:data="data" >
+                  <el-button type="danger" icon="el-icon-delete" size="small" ></el-button>
+                 </slot>
+                </td>
+              </tr>
+            </tbody>
+           
+          </table>
+        </div>
+      </div>
     </div>
     <div class="wenjian">
       <div class="wenjian-top">
@@ -130,14 +178,155 @@ export default {
           title: "编辑",
           width: 50,
           dataIndex: "k",
+          slot:1,
         },
         {
           title: "删除",
           width: 50,
           dataIndex: "l",
+          slot:2
         },
       ],
-      
+      dataSource:[{
+        No:'123',
+        Site:'234',
+        a:'12',
+        s:'123',
+        d:'123',
+        f:'213',
+        g:'141',
+        h:'1241',
+        j:'123',
+       
+      },
+      {
+        No:'123',
+        Site:'234',
+        a:'12',
+        s:'123',
+        d:'123',
+        f:'213',
+        g:'141',
+        h:'1241',
+        j:'123',
+       
+      }
+      ],
+      columns2:[
+         {
+          title: "No.",
+          width: 60,
+          dataIndex: "No",
+        },
+        {
+          title: "Site",
+          width: 60,
+          dataIndex: "Site",
+        },
+        {
+          title: "文件编号",
+          width: 200,
+          dataIndex: "a",
+        },
+        {
+          title: "文件名称",
+          width: 200,
+          dataIndex: "s",
+        },
+        {
+          title: "版本",
+          width: 100,
+          dataIndex: "d",
+        },
+        {
+          title: "发行日期",
+          width: 100,
+          dataIndex: "f",
+        },
+        {
+          title: "附件",
+          width: 50,
+          dataIndex: "g",
+        },
+        {
+          title: "状态",
+          width: 100,
+          dataIndex: "h",
+        },
+        {
+          title: "上传人",
+          width: 100,
+          dataIndex: "j",
+        },
+        {
+          title: "编辑",
+          width: 50,
+          dataIndex: "k",
+          slot:1
+        },
+        {
+          title: "删除",
+          width: 50,
+          dataIndex: "l",
+          slot:2
+        },
+      ],
+      dataSource2:[
+        {
+           No:'123',
+        Site:'234',
+        a:'12',
+        s:'123',
+        d:'123',
+        f:'213',
+        g:'141',
+        h:'1241',
+        j:'123',
+        },
+        {
+           No:'12',
+        Site:'234',
+        a:'12',
+        s:'123',
+        d:'123',
+        f:'213',
+        g:'141',
+        h:'1241',
+        j:'123',
+        },
+        {
+           No:'123',
+        Site:'234',
+        a:'12',
+        s:'123',
+        d:'13',
+        f:'213',
+        g:'141',
+        h:'1241',
+        j:'123',
+        }, {
+           No:'123',
+        Site:'234',
+        a:'12',
+        s:'123',
+        d:'123',
+        f:'213',
+        g:'141',
+        h:'1241',
+        j:'123',
+        },
+         {
+           No:'123',
+        Site:'234',
+        a:'12',
+        s:'123',
+        d:'123',
+        f:'213',
+        g:'141',
+        h:'1241',
+        j:'123',
+        },
+      ]
     };
   },
   mounted() {},
@@ -173,13 +362,7 @@ export default {
   display: flex;
   flex-direction: row;
   position: relative;
-}
-.title {
-  margin-right: auto;
-  width: 150px;
-  background-color: #60e21f;
-}
-.search-1 {
+  .search-1 {
   width: 146px;
   font-size: 12pt;
   cursor: pointer;
@@ -199,6 +382,13 @@ export default {
   font-size: 12pt;
   cursor: pointer;
 }
+}
+.title {
+  margin-right: auto;
+  width: 150px;
+  background-color: #60e21f;
+}
+
 .kuang1 {
   position: relative;
   background-image: url("./background/1280x120-邊框4px.png");
@@ -223,11 +413,81 @@ export default {
   font-size: 12pt;
 }
 .table-1 {
+  width: 1200px;
+  height: auto;
   text-align: center;
   font: 12pt "幼圆";
   line-height: 35px;
+   border-collapse:collapse;
   .th {
     padding-left: 10px;
   }
+  .body tr{
+    height: 35px;
+    width: 100%;
+  }
+  .tr-color-0{
+    background-color:rgba($color: #ffffff, $alpha: 0);
+  }
+  .tr-color-1{
+    background-color:rgba($color: #ffffff, $alpha: 0.5);
+  }
+ .body td{
+padding: 0%;
+margin: 0%;
+  color: #ffffff;
+    
+  }
+}
+
+
+.biaozhun{
+  display: flex;
+  flex-direction: column;
+
+  width: 1280px;
+  height: 320px;
+
+  position: absolute;
+  left: 50%;
+  top: 190px;
+  transform: translate(-50%, 0%);
+}
+.biaozhun-top{
+   height: 30px;
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  .search-2 {
+  width: 146px;
+  font-size: 12pt;
+  cursor: pointer;
+}
+.upload-2 {
+  margin-left: 20px;
+  font-size: 12pt;
+  cursor: pointer;
+}
+.export-2 {
+  margin-left: 20px;
+  font-size: 12pt;
+  cursor: pointer;
+}
+.new-2 {
+  margin-left: 20px;
+  font-size: 12pt;
+  cursor: pointer;
+}
+}
+.kuang2{
+ position: relative;
+  background-image: url("./background/1280x290 邊框4px-62-62.png");
+  width: 1280px;
+  height: 290px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -o-background-size: cover;
+  background-position: center 0;
 }
 </style>
